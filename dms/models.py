07fc13e401bd45e99e862e6cf640f3af5029c529
dennisjanvogt <dennisjanvogt@gmail.com>
2023-05-27 20:50:27 +0200

    updated dms

--- dms/models.py
+++ dms/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    docfile = models.FileField(upload_to="dms/documents/%Y/%m/%d/%H/%M/%S")
--- dms/views.py
+++ dms/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Document
+from .forms import DocumentForm
+from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+@login_required(login_url="login")
 def dms_index(request):
-    context = {}
+    if request.method == "POST":
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            title = form.cleaned_data.get("title")
+            newdoc = Document(title=title, docfile=request.FILES["docfile"])
+            newdoc.save()
 
-    render(request, "dms_index.html", context)
+            return redirect("dms_index")
+
+    else:
+        form = DocumentForm()
+
+    documents = Document.objects.all()
+
+    return render(request, "dms_index.html", {"documents": documents, "form": form})
