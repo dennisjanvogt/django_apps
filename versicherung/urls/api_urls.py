b3d2ec34bcd7af9edcf12381a4e9622eab744368
dennisjanvogt <dennisjanvogt@gmail.com>
2023-05-28 21:43:43 +0200

    some url missconceptions corrected

--- versicherung/urls/api_urls.py
+++ versicherung/urls/api_urls.py
@@ -26,47 +26,51 @@ from ..views.api_views import (
 
 urlpatterns = [
     # MITARBEITER
-    path("api/mitarbeiter/", mitarbeiter_list, name="mitarbeiter_list"),
-    path("api/mitarbeiter/create/", mitarbeiter_create, name="mitarbeiter_create"),
+    path("api/mitarbeiter/", mitarbeiter_list, name="api_mitarbeiter_list"),
+    path("api/mitarbeiter/create/", mitarbeiter_create, name="api_mitarbeiter_create"),
     path(
-        "api/mitarbeiter/<int:pk>/", mitarbeiter_retrieve, name="mitarbeiter_retrieve"
+        "api/mitarbeiter/<int:pk>/",
+        mitarbeiter_retrieve,
+        name="api_mitarbeiter_retrieve",
     ),
     path(
         "api/mitarbeiter/<int:pk>/update/",
         mitarbeiter_update,
-        name="mitarbeiter_update",
+        name="api_mitarbeiter_update",
     ),
     path(
         "api/mitarbeiter/<int:pk>/delete/",
         mitarbeiter_delete,
-        name="mitarbeiter_delete",
+        name="api_mitarbeiter_delete",
     ),
     # KUNDE
-    path("api/kunde/", kunde_list, name="kunde_list"),
-    path("api/kunde/create/", kunde_create, name="kunde_create"),
-    path("api/kunde/<int:pk>/", kunde_retrieve, name="kunde_retrieve"),
-    path("api/kunde/<int:pk>/update/", kunde_update, name="kunde_update"),
-    path("api/kunde/<int:pk>/delete/", kunde_delete, name="kunde_delete"),
+    path("api/kunde/", kunde_list, name="api_kunde_list"),
+    path("api/kunde/create/", kunde_create, name="api_kunde_create"),
+    path("api/kunde/<int:pk>/", kunde_retrieve, name="api_kunde_retrieve"),
+    path("api/kunde/<int:pk>/update/", kunde_update, name="api_kunde_update"),
+    path("api/kunde/<int:pk>/delete/", kunde_delete, name="api_kunde_delete"),
     # VERSICHERUNGSVERTRAG
-    path("api/versvertrag/", versvertrag_list, name="versvertrag_list"),
-    path("api/versvertrag/create/", versvertrag_create, name="versvertrag_create"),
+    path("api/versvertrag/", versvertrag_list, name="api_versvertrag_list"),
+    path("api/versvertrag/create/", versvertrag_create, name="api_ersvertrag_create"),
     path(
-        "api/versvertrag/<int:pk>/", versvertrag_retrieve, name="versvertrag_retrieve"
+        "api/versvertrag/<int:pk>/",
+        versvertrag_retrieve,
+        name="api_versvertrag_retrieve",
     ),
     path(
         "api/versvertrag/<int:pk>/update/",
         versvertrag_update,
-        name="versvertrag_update",
+        name="api_versvertrag_update",
     ),
     path(
         "api/versvertrag/<int:pk>/delete/",
         versvertrag_delete,
-        name="versvertrag_delete",
+        name="api_versvertrag_delete",
     ),
     # SCHADEN
-    path("api/schaden/", schaden_list, name="schaden_list"),
-    path("api/schaden/create/", schaden_create, name="schaden_create"),
-    path("api/schaden/<int:pk>/", schaden_retrieve, name="schaden_retrieve"),
-    path("api/schaden/<int:pk>/update/", schaden_update, name="schaden_update"),
-    path("api/schaden/<int:pk>/delete/", schaden_delete, name="schaden_delete"),
+    path("api/schaden/", schaden_list, name="api_schaden_list"),
+    path("api/schaden/create/", schaden_create, name="api_schaden_create"),
+    path("api/schaden/<int:pk>/", schaden_retrieve, name="api_schaden_retrieve"),
+    path("api/schaden/<int:pk>/update/", schaden_update, name="api_schaden_update"),
+    path("api/schaden/<int:pk>/delete/", schaden_delete, name="api_schaden_delete"),
 ]
